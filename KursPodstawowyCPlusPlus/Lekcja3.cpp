25088a322e9bd9d4e8455770a472080c065cdaa9
AndrzejRajczyk <rajczyk.andrzej@gmail.com>
2023-03-30 15:29:30 +0200

    Dodaj pliki projektów.

---
+++ KursPodstawowyCPlusPlus/Lekcja3.cpp
@@ -0,0 +1,31 @@
+
+/*
+zaproponuj zmiane kodu by kod dzia³a³
+
+int main()
+{
+    cout << "Znajdz dwa rozne rozwiazania tego zadania." << endl;
+    return 0;
+}*/
+
+
+
+/* Rozwi¹zanie nr 1 
+#include <iostream>
+using namespace std;
+int main() {
+
+    cout << "Znajdz dwa rozne rozwiazania tego zadania." << endl;
+
+    return 0;
+}
+*/
+/* Rozwi¹zanie nr 2 
+# include <iostream>
+int main() {
+
+    std::cout << "Znajdz dwa rozne rozwiazania tego zadania.";
+    std::cout << "\n";
+
+    return 0;
+}*/
