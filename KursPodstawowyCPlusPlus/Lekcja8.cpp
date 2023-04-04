bf9ce2615f6f6e943f08a7e73d766c601568f55d
AndrzejRajczyk <rajczyk.andrzej@gmail.com>
2023-04-04 10:29:27 +0200

    git add *.cpp

--- KursPodstawowyCPlusPlus/Lekcja8.cpp
+++ KursPodstawowyCPlusPlus/Lekcja8.cpp
@@ -4,9 +4,25 @@ int main()
 {
     bool porownanie = 123 >= 321;
 
-    cout << "porownanie = " << porownanie << endl;
-    cout << "porownanie = " << (111 != 222) << endl;
-    cout << "porownanie = " << (777 == 777) << endl;
+    cout << "porownanie = "<< endl;
+    cout << "porownanie = " << (111 != 222) << endl; 
+    cout << "porownanie = " << (777 == 777) << endl; 
     cout << "porownanie = " << (777 < 777) << endl;
     return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
